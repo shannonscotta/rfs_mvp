5f69ddf78ab7f1bf55608747cb32c506c67a4c90
Scott Shannon <78231948+shannonscotta@users.noreply.github.com>
2023-11-18 20:56:21 -0800

    style header icons

--- client/components/Navigation/Navigation.jsx
+++ client/components/Navigation/Navigation.jsx
@@ -1,12 +1,12 @@
 import GithubIcon from "../../assets/GithubIcon";
 import LinkedinIcon from "../../assets/LinkedinIcon";
-import NavigationCSS from "./Navigation.module.css"
+import NavigationCSS from "./Navigation.module.css";
 
 const Navigation = () => {
   return (
     <header className={NavigationCSS.header}>
-      <GithubIcon fillColor={"#CECECE"} />
-      <LinkedinIcon fillColor={"#CECECE"} />
+      <GithubIcon />
+      <LinkedinIcon />
     </header>
   );
 };
