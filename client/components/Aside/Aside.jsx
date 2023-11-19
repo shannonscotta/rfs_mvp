f7e50f5e50a2b7c6cfb655709a9442a6d578196d
Scott Shannon <78231948+shannonscotta@users.noreply.github.com>
2023-11-19 12:38:01 -0800

    add aside svgs and styling

--- client/components/Aside/Aside.jsx
+++ client/components/Aside/Aside.jsx
@@ -1,9 +1,19 @@
+import HomeIcon from "../../assets/HomeIcon";
+import CodeIcon from "../../assets/CodeIcon";
+
+import MailIcon from "../../assets/MailIcon";
 import AsideCSS from "./Aside.module.css";
 
 const Aside = () => {
-    return (
-        <aside className={AsideCSS.aside}></aside>
-    )
-}
+  return (
+    <aside className={AsideCSS.aside}>
+      <section>
+        <HomeIcon />
+        <CodeIcon />
+        <MailIcon />
+      </section>
+    </aside>
+  );
+};
 
 export default Aside;
